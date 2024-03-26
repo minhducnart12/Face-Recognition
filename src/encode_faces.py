229802e80fe0c399d4daa35af725bb6db9cd77ac
minhducnart12 <minhduc.nart.12@gmail.com>
2024-03-27 03:41:58 +0700

    feat: Add main() function to run the program from the command line

--- src/encode_faces.py
+++ src/encode_faces.py
@@ -5,52 +5,60 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import sys
+import pickle
 import tensorflow as tf
 
+def parse_arguments(argv):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--dataset", type=str, default='../Datasets/Pictures/Normalised',
+                    help="path to Norm/dir")
+    return ap.parse_args(argv)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", type=str, default='Datasets/Pictures/Normalised',
-                help="path to Norm/dir")
-# ap.add_argument("-o", "--save", type=str, default='/Models/model.h5',
-#                 help="path to save .h5 model")
+def main():
+    # Load ArcFace Model
+    parsed_args = parse_arguments(sys.argv[1:])
+    path_to_dir = parsed_args.dataset
+    model = ArcFace.loadModel()
+    model.load_weights("../Models/arcface_weights.h5")
+    print("ArcFace expects ", model.layers[0].input_shape[0][1:], " inputs")
+    print("and it represents faces as ",
+        model.layers[-1].output_shape[1:], " dimensional vectors")
+    target_size = model.layers[0].input_shape[0][1:3]
+    print('target_size: ', target_size)
 
+    # Variable for store img Embedding
+    x = []
+    y = []
 
-args = vars(ap.parse_args())
-path_to_dir = args["dataset"]
-# checkpoint_path = args['save']
+    names = os.listdir(path_to_dir)
+    names = sorted(names)
 
-# Load ArcFace Model
-model = ArcFace.loadModel()
-model.load_weights("Models/arcface_weights.h5")
-print("ArcFace expects ", model.layers[0].input_shape[0][1:], " inputs")
-print("and it represents faces as ",
-      model.layers[-1].output_shape[1:], " dimensional vectors")
-target_size = model.layers[0].input_shape[0][1:3]
-print('target_size: ', target_size)
+    for name in names:
+        img_list = glob.glob(os.path.join(path_to_dir, name) + '/*')
+        img_list = sorted(img_list)
 
-# Variable for store img Embedding
-x = []
-y = []
+        for img_path in img_list:
+            img = cv2.imread(img_path)
+            img_resize = cv2.resize(img, target_size)
 
-names = os.listdir(path_to_dir)
-names = sorted(names)
-class_number = len(names)
+            img_pixels = tf.keras.preprocessing.image.img_to_array(img_resize)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_norm = img_pixels/255  # normalize input in [0, 1]
+            img_embedding = model.predict(img_norm)[0]
 
-for name in names:
-    img_list = glob.glob(os.path.join(path_to_dir, name) + '/*')
-    img_list = sorted(img_list)
+            x.append(img_embedding)
+            y.append(name)
+            print(f'[INFO] Embedding {img_path}')
+        print(f'[INFO] Completed {name} Part')
+    print('[INFO] Image Data Embedding Completed...')
 
-    for img_path in img_list:
-        img = cv2.imread(img_path)
-        img_resize = cv2.resize(img, target_size)
+    pickle_file_path = "../Models/EncodeFile.pkl"
 
-        img_pixels = tf.keras.preprocessing.image.img_to_array(img_resize)
-        img_pixels = np.expand_dims(img_pixels, axis=0)
-        img_norm = img_pixels/255  # normalize input in [0, 1]
-        img_embedding = model.predict(img_norm)[0]
+    with open(pickle_file_path, 'wb') as f:
+        pickle.dump((x, y), f)
 
-        x.append(img_embedding)
-        y.append(name)
-        print(f'[INFO] Embedding {img_path}')
-    print(f'[INFO] Completed {name} Part')
-print('[INFO] Image Data Embedding Completed...')
+    print(f'[INFO] Embeddings saved to {pickle_file_path}')
+
+if __name__ == "__main__":
+    main()
--- src/take_images.py
+++ src/take_images.py
@@ -1,87 +1,94 @@
 import os
 import cv2
+import sys
 import argparse
 
+def parse_arguments(argv):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--source", type=str, required=True,
+                    help="Video folder path or webcam-id")
+    ap.add_argument("-n", "--name", type=str, required=True,
+                    help="name of the person")
+    ap.add_argument("-o", "--save", type=str, default='../Datasets/Pictures/Raw',
+                    help="path to save dir")
+    ap.add_argument("-c", "--conf", type=float, default=0.5,
+                    help="min prediction conf (0<conf<1)")
+    ap.add_argument("-x", "--number", type=int, default=100,
+                    help="number of data wants to collect")
+    
+    return ap.parse_args(argv)
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--source", type=str, required=True,
-                help="Video folder path or webcam-id")
-ap.add_argument("-n", "--name", type=str, required=True,
-                help="name of the person")
-ap.add_argument("-o", "--save", type=str, default='Data',
-                help="path to save dir")
-ap.add_argument("-c", "--conf", type=float, default=0.5,
-                help="min prediction conf (0<conf<1)")
-ap.add_argument("-x", "--number", type=int, default=100,
-                help="number of data wants to collect")
+def main():
+    # Load ArcFace Model
+    parsed_args = parse_arguments(sys.argv[1:])
+    source = parsed_args.source
+    name_of_person = parsed_args.name
+    path_to_save = parsed_args.save
+    min_confidence = parsed_args.conf
 
+    os.makedirs((os.path.join(path_to_save, name_of_person)), exist_ok=True)
+    path_to_save = os.path.join(path_to_save, name_of_person)
 
-args = vars(ap.parse_args())
-source = args["source"]
-name_of_person = args['name']
-path_to_save = args['save']
-min_confidence = args["conf"]
-
-os.makedirs((os.path.join(path_to_save, name_of_person)), exist_ok=True)
-path_to_save = os.path.join(path_to_save, name_of_person)
-
-opencv_dnn_model = cv2.dnn.readNetFromCaffe(
-    prototxt="models/deploy.prototxt",
-    caffeModel="models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
-)
-
-if source.isnumeric():
-    source = int(source)
-cap = cv2.VideoCapture(source)
-fps = cap.get(cv2.CAP_PROP_FPS)
-
-count = 0
-img_name = 0
-while True:
-    success, img = cap.read()
-    if not success:
-        print('[INFO] Cam NOT working!!')
-        break
-
-    # Caffe Model - Face Detection
-    h, w, _ = img.shape
-    preprocessed_image = cv2.dnn.blobFromImage(
-        img, scalefactor=1.0, size=(300, 300),
-        mean=(104.0, 117.0, 123.0), swapRB=False, crop=False
+    opencv_dnn_model = cv2.dnn.readNetFromCaffe(
+        prototxt="../Models/deploy.prototxt",
+        caffeModel="../Models/res10_300x300_ssd_iter_140000_fp16.caffemodel"
     )
-    opencv_dnn_model.setInput(preprocessed_image)
-    results = opencv_dnn_model.forward() 
 
-    face_count = 0
-    for face in results[0][0]:
+    if source.isnumeric():
+        source = int(source)
+    cap = cv2.VideoCapture(source)
+    fps = cap.get(cv2.CAP_PROP_FPS)
 
-        face_confidence = face[2]
-        if face_confidence > min_confidence:
-            face_count += 1
-            bbox = face[3:]
-            img_without_rect = img.copy()
-            x1 = int(bbox[0] * w)
-            y1 = int(bbox[1] * h)
-            x2 = int(bbox[2] * w)
-            y2 = int(bbox[3] * h)
-            cv2.rectangle(
-                img, pt1=(x1, y1), pt2=(x2, y2),
-                color=(0, 255, 0), thickness=w//200
-            )
-        
-    # Save Image
-    if  count % int(fps/5) == 0:
-        if face_count == 1:
-            img_name = len(os.listdir(path_to_save))
-            cv2.imwrite(f'{path_to_save}/{img_name}.jpg',img_without_rect)
-            print(f'[INFO] Successfully Saved {img_name}.jpg')
-        else:
-            print(f'[WARNING]More than one face appears')
-    count += 1
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+    count = 0
+    img_name = 0
+    while True:
+        success, img = cap.read()
+        if not success:
+            print('[INFO] Cam NOT working!!')
+            break
 
-    if img_name == args["number"]-1:
-        print(f"[INFO] Collected {args['number']} Images")
-        cv2.destroyAllWindows()
-        break
+        # Caffe Model - Face Detection
+        h, w, _ = img.shape
+        preprocessed_image = cv2.dnn.blobFromImage(
+            img, scalefactor=1.0, size=(300, 300),
+            mean=(104.0, 117.0, 123.0), swapRB=False, crop=False
+        )
+        opencv_dnn_model.setInput(preprocessed_image)
+        results = opencv_dnn_model.forward() 
+
+        face_count = 0
+        for face in results[0][0]:
+
+            face_confidence = face[2]
+            if face_confidence > min_confidence:
+                face_count += 1
+                bbox = face[3:]
+                img_without_rect = img.copy()
+                x1 = int(bbox[0] * w)
+                y1 = int(bbox[1] * h)
+                x2 = int(bbox[2] * w)
+                y2 = int(bbox[3] * h)
+                cv2.rectangle(
+                    img, pt1=(x1, y1), pt2=(x2, y2),
+                    color=(0, 255, 0), thickness=w//200
+                )
+            
+        # Save Image
+        if  count % int(fps/5) == 0:
+            if face_count == 1:
+                img_name = len(os.listdir(path_to_save))
+                cv2.imwrite(f'{path_to_save}/{img_name}.jpg',img_without_rect)
+                print(f'[INFO] Successfully Saved {img_name}.jpg')
+            else:
+                print(f'[WARNING]More than one face appears')
+        count += 1
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
+
+        if img_name == parsed_args.number-1:
+            print(f"[INFO] Collected {parsed_args.number} Images")
+            cv2.destroyAllWindows()
+            break
+
+if __name__ == "__main__":
+    main()
